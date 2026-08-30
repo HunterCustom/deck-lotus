80df6619c3f179828f0979f0d32f97613a3fa126
HunterCustom <hunterfkbo2@gmail.com>
2026-08-30 10:21:57 -0400

    Add shared HTML escaping helper

--- client/src/utils/ui.js
+++ client/src/utils/ui.js
@@ -1,3 +1,13 @@
+export function escapeHtml(value) {
+  return String(value ?? '').replace(/[&<>"']/g, (char) => ({
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+  })[char]);
+}
+
 export function showLoading() {
   document.getElementById('loading').classList.remove('hidden');
 }
